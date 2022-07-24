1f3c0ea60be995c34e5cd8439bc6cb731e82d940
Demon_Venerable <54703353+EpicCoderBoi@users.noreply.github.com>
2022-07-25 02:19:34 +0700

    Add files via upload

---
+++ Application/PublicVariables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EYE.AI
+{
+    public static class PublicVariables
+    {
+        public static string EyeImageURL { get; set; }
+
+        public static bool EyeDiseaseAPI { get; set; }
+
+        public static bool CornealUlcersAPI { get; set; }
+
+        public static bool BlindnessAPI { get; set; }
+    }
+}
